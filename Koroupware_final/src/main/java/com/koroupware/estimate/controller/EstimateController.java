f73630af6cd14fb241573bcb9c21cbdbc87c0c4b
sdc337dc <sdc337dc@DESKTOP-F79GR1G>
2016-12-05 15:12:39 +0900

    다시다시다시다시

--- Koroupware_final/src/main/java/com/koroupware/estimate/controller/EstimateController.java
+++ Koroupware_final/src/main/java/com/koroupware/estimate/controller/EstimateController.java
@@ -32,6 +32,178 @@ public class EstimateController {
 		return "/estimate/est_start";
 		
 	}
+	
+	
+	@RequestMapping(value = "/lateCount", method = RequestMethod.POST)
+	public String lateCount(AttendVO attendVO, Model model, EstimateVO estimateVO) throws Exception {
+		int dept_no =0;//부서
+		if(estimateVO.getKeyField().equals("dept_1")){
+				dept_no=1;
+			}else if(estimateVO.getKeyField().equals("dept_2")){
+				dept_no=2;
+			}else if(estimateVO.getKeyField().equals("dept_3")){
+				dept_no=3;
+			}else if(estimateVO.getKeyField().equals("dept_4")){
+				dept_no=4;
+			}else{
+				dept_no=5;
+			}
+		
+	
+		
+		AttendAccountUtils totalResult = new AttendAccountUtils();
+		ResultVO resultVO = new ResultVO();
+		List<EmpDTO> attendDept_EmpList = service.attendDept_EmpList(dept_no);
+		List<ResultVO> AttendQuaterList = service.AttendQuaterList();
+		System.out.println(AttendQuaterList);
+
+		   
+		int emp_count = service.empCount(dept_no); // 해당 부서에 인원 수 파악	
+		
+		int year = 2016;
+		int resultTotal =0;
+		double account =0; // 총일수, 출석 수 계산
+		double first_quater =0; // account를 3으로 나누어 분기 설정
+		double secont_quater =0;
+		double third_quater=0;
+		double forth_quater=0;
+		double final_quater=0;
+		double future_quater=0;
+		
+		
+		for(int k= 0; k<emp_count; k++){
+			attendVO.setEmp_no(attendDept_EmpList.get(k).getEmp_no());
+			System.out.println("사원 번호 : "+ attendDept_EmpList.get(k).getEmp_no());
+			
+			//1분기
+			for(int i=1; i<=3; i++){
+			System.out.println(attendDept_EmpList.get(k).getEmp_no());
+			attendVO.count(attendDept_EmpList.get(k).getEmp_no(), year, i);
+
+			int count = service.attendCount(attendVO);
+			
+			resultTotal = totalResult.dateTotal(year, i);
+			System.out.println((double)count);
+			System.out.println((double)resultTotal);
+			double a = (double)count/ (double)resultTotal;
+			
+			// 1분기 계산
+			account = totalResult.round(a,3)*100;
+			System.out.println(i+"월  확률 :" + account);
+			
+			first_quater= first_quater + account;	
+			System.out.println("현재 가지고 있는 분기 점수 : "+ first_quater);
+		}
+		
+
+		//2분기
+		for(int i=4; i<=6; i++){
+			attendVO.count(attendDept_EmpList.get(k).getEmp_no(), year, i);
+
+			int count = service.attendCount(attendVO);
+			
+			resultTotal = totalResult.dateTotal(year, i);
+			double a = (double)count/ (double)resultTotal;
+			
+			// 1분기 계산
+			account = totalResult.round(a,3)*100;
+			secont_quater= secont_quater + account;
+			System.out.println(i+"월  확률 :" + account);
+		}
+
+		
+		//3분기
+		for(int i=7; i<=9; i++){
+			attendVO.count(attendDept_EmpList.get(k).getEmp_no(), year, i);
+
+			int count = service.attendCount(attendVO);
+			
+			resultTotal = totalResult.dateTotal(year, i);
+			double a = (double)count/ (double)resultTotal;
+			
+			// 1분기 계산
+			account = totalResult.round(a,3)*100;
+			third_quater= third_quater + account;
+			System.out.println(i+"월  확률 :" + account);
+		}
+
+		//4분기
+		for(int i=10; i<=12; i++){
+			attendVO.count(attendDept_EmpList.get(k).getEmp_no(), year, i);
+
+			int count = service.attendCount(attendVO);
+			
+			resultTotal = totalResult.dateTotal(year, i);
+			double a = (double)count/ (double)resultTotal;
+			
+			// 1분기 계산
+			account = totalResult.round(a,3)*100;
+			forth_quater= forth_quater + account;
+			System.out.println(i+"월  확률 :" + account);
+		}
+		
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 1분기 총합은???"+ first_quater);
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+ "의 1분기 마무리 : "+first_quater/3);
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 2분기 총합은???"+ secont_quater);
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 2분기 마무리 : "+secont_quater/3);
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 2분기 총합은???"+ third_quater);
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 2분기 마무리 : "+third_quater/3);
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 2분기 총합은???"+ forth_quater);
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 2분기 마무리 : "+forth_quater/3);
+		System.out.println("-------------------------------");
+		System.out.println(" ");
+		
+		
+		future_quater = (((first_quater/3)-(secont_quater/3))+((secont_quater/3)-(third_quater/3))+((third_quater/3)-(forth_quater/3)))/3;
+		final_quater = ((first_quater/3)+(secont_quater/3)+(third_quater/3)+(forth_quater/3))/4;
+		future_quater = final_quater+ future_quater;
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 예측값 : "+future_quater);
+		System.out.println(attendDept_EmpList.get(k).getEmp_no()+"의 최종 평균 : "+final_quater);
+		
+		if(AttendQuaterList.size()==0){
+			resultVO.setQuarterfirst(first_quater/3);
+			resultVO.setQuartersecond(secont_quater/3);
+			resultVO.setQuarterthird(third_quater/3);
+			System.out.println("데이터 넣음");
+			resultVO.setQuarterforth(forth_quater/3);
+			resultVO.setFinalquarter(final_quater);
+			resultVO.setFuture_quater(future_quater);
+			resultVO.setEmp_no(attendDept_EmpList.get(k).getEmp_no());
+			resultVO.setResult_year(year);
+			service.insertAttendQuater(resultVO);
+		}else{
+			for(int x=0; x<AttendQuaterList.size(); x++){
+				if((AttendQuaterList.get(x).getEmp_no()!=attendDept_EmpList.get(k).getEmp_no()&&AttendQuaterList.get(x).getResult_year()!=year)){
+					resultVO.setQuarterfirst(first_quater/3);
+					resultVO.setQuartersecond(secont_quater/3);
+					resultVO.setQuarterthird(third_quater/3);
+					System.out.println("데이터 넣음");
+					resultVO.setQuarterforth(forth_quater/3);
+					resultVO.setFinalquarter(final_quater);
+					resultVO.setFuture_quater(future_quater);
+					resultVO.setEmp_no(attendDept_EmpList.get(k).getEmp_no());
+					resultVO.setResult_year(year);
+					service.insertAttendQuater(resultVO);
+				}else{
+					System.out.println("이미 넣었음");
+					
+				}
+		}
+		
+		}
+		first_quater=0;
+		secont_quater=0;
+		third_quater=0;
+		forth_quater=0;
+		final_quater=0;
+		future_quater=0;
+		}
+		//model.addAttribute("AttendQuaterList", AttendQuaterList);
+		return "redirect:/estimate/estimate_output";
+	}
+	
+	
+	
 	@RequestMapping(value = "/attendCount", method = RequestMethod.POST)
 	public String attendCount(AttendVO attendVO, Model model, EstimateVO estimateVO) throws Exception {
 		int dept_no =0;//부서
--- Koroupware_final/src/main/webapp/WEB-INF/views/estimate/attend_est_table.jsp
+++ Koroupware_final/src/main/webapp/WEB-INF/views/estimate/attend_est_table.jsp
@@ -106,5 +106,87 @@ $(function() {
 		</c:forEach>
 		
 		</table>
+		
+		<div class="col-md-6">
+		<form action="/estimate/attendCount/" method="post">
+			<div class="col-md-12 header-est-div">
+				<div class="col-md-4 header-est">
+					<span class="header-est-span">출석 인사평가</span><br>
+					<select name="keyField">
+					<option value="dept_1">1부서</option>
+					<option value="dept_2">2부서</option>
+					<option value="dept_3">3부서</option>
+					<option value="dept_4">4부서</option>
+					<option value="dept_5">5부서</option>
+				</select>
+				</div>
+				<div class="col-md-2">
+				<input type="submit" value="출석 평가">
+				</div>
+			</div>
+				</form>
+		</div>
+		
+<div class="col-md-6">
+		<form action="/estimate/lateCount/" method="post">
+			<div class="col-md-12 header-est-div">
+				<div class="col-md-4 header-est">
+					<span class="header-est-span">지각 인사평가</span><br>
+					<select name="keyField">
+					<option value="dept_1">1부서</option>
+					<option value="dept_2">2부서</option>
+					<option value="dept_3">3부서</option>
+					<option value="dept_4">4부서</option>
+					<option value="dept_5">5부서</option>
+				</select>
+				</div>
+				<div class="col-md-2">
+				<input type="submit" value="지각 평가">
+				</div>
+			</div>
+				</form>
+		</div>
+
+<div class="col-md-6">
+		<form action="/estimate/specialCount/" method="post">
+			<div class="col-md-12 header-est-div">
+				<div class="col-md-4 header-est">
+					<span class="header-est-span">특근 인사평가</span><br>
+					<select name="keyField">
+					<option value="dept_1">1부서</option>
+					<option value="dept_2">2부서</option>
+					<option value="dept_3">3부서</option>
+					<option value="dept_4">4부서</option>
+					<option value="dept_5">5부서</option>
+				</select>
+				</div>
+				<div class="col-md-2">
+				<input type="submit" value="특근 평가">
+				</div>
+			</div>
+				</form>
+		</div>
+		<br>
+		<br>
+
+<div class="col-md-6">
+		<form action="/estimate/totalCount/" method="post">
+			<div class="col-md-12 header-est-div">
+				<div class="col-md-4 header-est">
+					<span class="header-est-span">총합 인사평가</span><br>
+					<select name="keyField">
+					<option value="dept_1">1부서</option>
+					<option value="dept_2">2부서</option>
+					<option value="dept_3">3부서</option>
+					<option value="dept_4">4부서</option>
+					<option value="dept_5">5부서</option>
+				</select>
+				</div>
+				<div class="col-md-2">
+				<input type="submit" value="종합 평가">
+				</div>
+			</div>
+				</form>
+		</div>						
 </body>
 </html>
